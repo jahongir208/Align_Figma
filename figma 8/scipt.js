37152f0810db5ddc49e3436fa68a7f459d2f6b50
jahongir208 <152614710+jahongir208@users.noreply.github.com>
2024-07-30 10:58:52 +0500

    Add files via upload

---
+++ figma 8/scipt.js
@@ -0,0 +1,19 @@
+// let btn = document.querySelector(".enter")
+let list = document.querySelector(".mix3")
+// let x = document.querySelector(".mini-icon i")
+
+
+// btn.addEventListener("click", () => {
+//     list.classList.add("open")
+// })
+
+// x.addEventListener("click", () => {
+//     list.classList.add("close")
+// })
+function start(){
+    list.style.display="block"
+}
+function hide(){
+    list.style.display="none"
+
+}
